9cbc23a504d506862a835bb4a4b36c30a16d5709
ray-cod <raimilassissi8@gmail.com>
2025-08-20 12:52:19 +0200

    Initial setup

---
+++ src/main/java/com/reservation/online_reservation_system/OnlineReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.online_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservation/online_reservation_system/OnlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.online_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
