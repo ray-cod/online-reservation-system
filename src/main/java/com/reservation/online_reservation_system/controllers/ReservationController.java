c6d6f847f4093c83b9879976dbd03f850a43e0c9
ray-cod <raimilassissi8@gmail.com>
2025-08-25 12:56:14 +0200

    Created reservation controller.

--- src/main/java/com/reservation/online_reservation_system/controllers/ReservationController.java
+++ src/main/java/com/reservation/online_reservation_system/controllers/ReservationController.java
@@ -1,4 +1,55 @@
 package com.reservation.online_reservation_system.controllers;
 
+import com.reservation.online_reservation_system.models.Reservation;
+import com.reservation.online_reservation_system.models.User;
+import com.reservation.online_reservation_system.services.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    // Book a reservation
+    @PostMapping("/book")
+    public ResponseEntity<Reservation> bookReservation(@RequestBody Reservation reservation) {
+        Reservation savedReservation = reservationService.bookReservation(reservation);
+        return ResponseEntity.ok(savedReservation);
+    }
+
+    // Cancel a reservation
+    @PutMapping("/cancel/{pnrNumber}")
+    public ResponseEntity<Reservation> cancelReservation(@PathVariable Long pnrNumber) {
+        Reservation cancelledReservation = reservationService.cancelReservation(pnrNumber);
+        return ResponseEntity.ok(cancelledReservation);
+    }
+
+    // Get all reservations
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        return ResponseEntity.ok(reservationService.getAllReservations());
+    }
+
+    // Get reservations by userId
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Reservation>> getReservationsByUser(@PathVariable Long userId) {
+        User user = new User();
+        user.setId(userId);
+        return ResponseEntity.ok(reservationService.getReservationsByUser(user));
+    }
+
+    // Get reservation by PNR
+    @GetMapping("/{pnrNumber}")
+    public ResponseEntity<Reservation> getReservationByPnr(@PathVariable Long pnrNumber) {
+        return reservationService.getReservationByPnr(pnrNumber)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
+
